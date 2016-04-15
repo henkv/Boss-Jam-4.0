d3a76cacc76ea250c405405ed25d6b7f37c4a734
Maakuz <markuswerdien@hotmail.com>
2016-04-16 00:11:16 +0200

    Scene

---
+++ Assets/PotBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PotBehaviour : MonoBehaviour
+{
+    public bool potActivated;
+    Rigidbody2D rigidbody;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        potActivated = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (potActivated)
+        {
+
+        }
+	}
+}
