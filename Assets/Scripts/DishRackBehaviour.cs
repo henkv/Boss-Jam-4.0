0388775032945ae1f0a95499829f76b7061820e8
Maakuz <markuswerdien@hotmail.com>
2016-04-16 01:35:57 +0200

    skript&bakgrund

---
+++ Assets/Scripts/DishRackBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DishRackBehaviour : MonoBehaviour
+{
+    public bool rackActivated;
+    private Rigidbody2D rigidbody;
+    private float startPos;
+    public float distance;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rackActivated = false;
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.isKinematic = true;
+
+        startPos = transform.position.x;
+        distance = 0.3f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (rackActivated)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(new Vector2(-100, 0));
+            rackActivated = false;
+        }
+
+        if (transform.position.x < startPos - distance)
+        {
+            rigidbody.isKinematic = true;
+        }
+    }
+
+    public void trigger()
+    {
+        rackActivated = true;
+    }
+}
---
+++ Assets/Scripts/PlateBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateBehaviour : MonoBehaviour
+{
+    public bool plateActivated;
+    private Rigidbody2D rigidbody;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (plateActivated)
+        {
+            rigidbody.isKinematic = false;
+            plateActivated = false;
+        }
+	}
+}
