4e130cc00bb350d94815361b04388e4d282094c7
Henrik Vik <henkvik@gmail.com>
2016-04-16 20:09:56 +0200

    hrdn

---
+++ Assets/TileBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileBehaviour : MonoBehaviour {
+	public float xMin;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < xMin) {
+			Object.Destroy (gameObject);		
+		}
+	}
+}
