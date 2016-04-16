d5ef191a749191c917418d95767bd80737855633
Henrik Vik <henkvik@gmail.com>
2016-04-16 21:51:26 +0200

    ikmregimeiohnuoetei

--- Assets/Scripts/ScoreKeeperBehaviour.cs
+++ Assets/Scripts/ScoreKeeperBehaviour.cs
@@ -14,18 +14,18 @@ public class ScoreKeeperBehaviour : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		pairBehaviour = player.GetComponent<PairBehaviour> ();
-		score = 0;
-		runTime = 0;
+		score = 20;
+		runTime = 20;
+		loveLevel = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (pairBehaviour.inLove) {
-			score += Time.deltaTime;
+		if (!pairBehaviour.inLove) {
+			runTime -= Time.deltaTime;
 		}
 
-		runTime += Time.deltaTime;
-		loveLevel = score / runTime;
+		loveLevel = runTime / score;
 
 	}
 }
---
+++ Assets/Scripts/WallOfDeathBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallOfDeathBehaviour : MonoBehaviour {
+	public GameObject scoreKeeper;
+
+	private ScoreKeeperBehaviour skb;
+	// Use this for initialization
+	void Start () {
+		skb = scoreKeeper.GetComponent<ScoreKeeperBehaviour> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (skb.loveLevel < 0) 
+		{
+			Loose ();
+		}
+	}
+
+	void Loose()
+	{
+		Application.LoadLevel (1);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D hit)
+	{
+		Debug.Log (hit.name);
+		Loose ();
+	}
+}
