99c5c445901a0fd5d06d7aa418fe3656689be2db
Maakuz <markuswerdien@hotmail.com>
2016-04-16 21:42:58 +0200

    scener och skript

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+    public GUIStyle style;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void OnGUI()
+    {
+        if (GUI.Button(new Rect(0, 0, Screen.width, Screen.height), "CLICK TO CONTINUE", style))
+            startGame();
+    }
+
+    public void startGame()
+    {
+        Application.LoadLevel("TitleScene");
+        Debug.Log("Boop");
+    }
+
+
+
+}
